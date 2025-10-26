aba1312e295f3d600a4b2a2b6f7e0a7238b9ed9e
alphamunene <alpha.muneeno@strathmore.edu>
2025-10-26 22:48:03 +0300

    changes

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
   <style>
     body {
-      background-color: #b2e4fe;
+      background-color: #011f42;
       font-family: 'Segoe UI', sans-serif;
     }
     .card {
